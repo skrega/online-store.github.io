78e386d908a8642822747ecaba5fac64693fbe7e
Regina <49993734+skrega@users.noreply.github.com>
2021-10-30 19:46:34 +1000

    Add files via upload
    upload second homework

--- js/main.js
+++ js/main.js
@@ -1,43 +1,91 @@
-const products = [{
-        id: 1,
-        title: 'NoteBook',
-        price: 2000,
-        image: 'images/pic.jpg'
-    },
-    {
-        id: 2,
-        title: 'Mouse',
-        price: 20,
-        image: 'images/pic.jpg'
-    },
-    {
-        id: 3,
-        title: 'Keyboard',
-        price: 200,
-        image: 'images/pic.jpg'
-    },
-    {
-        id: 4,
-        title: 'Gamepad',
-        price: 50,
-        image: 'images/pic.jpg'
-    },
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render(); //вывод товаров на страницу
+        this.sumGoods = 0;
+        this.getSum();
+    }
+    _fetchProducts() {
+        this.goods = [{
+                id: 1,
+                title: 'NoteBook',
+                price: 2000,
+                img: 'https://via.placeholder.com/200x150'
+            },
+            {
+                id: 2,
+                title: 'Mouse',
+                price: 20,
+                img: 'https://via.placeholder.com/200x150'
+            },
+            {
+                id: 3,
+                title: 'Keyboard',
+                price: 200,
+                img: 'https://via.placeholder.com/200x150'
+            },
+            {
+                id: 4,
+                title: 'Gamepad',
+                price: 50,
+                img: 'https://via.placeholder.com/200x150'
+            },
+        ]
+    }
+    getSum() {
+        for (let i = 0; i < this.goods.length; i++) {
+            this.sumGoods += this.goods[i].price;
+        }
+        console.log(`Общая стоимость всех товаров составляет ${this.sumGoods}`);
+    }
 
-]
-const renderProduct = (item) =>
-    `<div class="product-item">
-        <img src="${item.image}" class="product-item__img" alt="${item.title}">
-        <h3>${item.title}</h3>
-        <p>${item.price}</p>
-        <button class="btn buy-btn">Купить</button>
-    </div>`;
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
 
-const renderPage = list => {
-    const productList = list.map(item => renderProduct(item));
-    console.log(productList.join(''));
-    document.querySelector('.products').innerHTML = productList.join('');
 }
 
-console.log(products);
 
-renderPage(products);
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = product.img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}" class="product-item__img" alt="${this.title}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="btn buy-btn">Купить</button>
+            </div>`
+    }
+
+}
+
+let list = new ProductList();
+
+
+class AddedCart {
+    constructor(container = '.buy-btn') {
+        this.addProduct();
+    }
+
+
+}
+
+class Cart {
+    constructor(container = '.btn-cart') {
+        this.removeProduct();
+        this.updateCart();
+
+    }
+}
